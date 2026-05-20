562f5124fc1cc8effe645f6ff0e45f42b29af96b
Aspas <aguiarmatheusalves09@gmail.com>
2026-05-20 16:29:52 -0300

    Bagulho importante, coloca

---
+++ sistema.py
@@ -0,0 +1,14 @@
+from typing import Self
+
+
+class Conta:
+    def __init__(self):
+        self.saldo = 0
+
+    def sacar_money(sefl, dinheiro_retirado):
+        dinheiro = dinheiro_retirado
+        self -= dinheiro
+    
+    def depositar_money(self, dinheiro_retirado):
+        dinheiro = dinheiro_retirado
+        self.saldo 
