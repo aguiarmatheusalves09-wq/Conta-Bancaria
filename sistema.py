7c7c4c2015a91fd48df532ed679bca46241b29aa
Aspas <aguiarmatheusalves09@gmail.com>
2026-05-20 16:37:38 -0300

    bla bla

--- sistema.py
+++ sistema.py
@@ -3,12 +3,17 @@ from typing import Self
 
 class Conta:
     def __init__(self):
-        self.saldo = 0
+        self.__saldo = 0
 
-    def sacar_money(sefl, dinheiro_retirado):
+    def sacar_money(self, dinheiro_retirado):
         dinheiro = dinheiro_retirado
-        self -= dinheiro
+        self.__saldo -= dinheiro
     
     def depositar_money(self, dinheiro_retirado):
         dinheiro = dinheiro_retirado
-        self.saldo 
+        self.saldo += dinheiro
+
+    def rendimento_do_money(self, meses):
+        meses = meses
+        while range(meses):
+            self.__saldo += self.__saldo * 0.1
